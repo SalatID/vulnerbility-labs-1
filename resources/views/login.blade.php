dd25a79eb4bf21407d8458f9fcd002d0843deb58
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2025-06-11 15:45:56 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,13 +12,13 @@
     <title>SB Admin 2 - Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('template')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ env('APP_URL') }}/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{asset('template')}}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ env('APP_URL') }}/template/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -81,14 +81,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('template')}}/vendor/jquery/jquery.min.js"></script>
-    <script src="{{asset('template')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ env('APP_URL') }}/template/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ env('APP_URL') }}/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('template')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ env('APP_URL') }}/template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('template')}}/js/sb-admin-2.min.js"></script>
+    <script src="{{ env('APP_URL') }}/template/js/sb-admin-2.min.js"></script>
 
 </body>
 
